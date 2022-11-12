fd9ca8edd1ed6b8bef47fa501326227f19115d19
Bottersnike <bottersnike237@gmail.com>
2022-11-12 13:38:33 +0000

    Setup scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Tweet from "./components/Tweet";
 
 const App = () => {
-    return <div />;
+    return (
+        <div>
+            <Tweet />
+        </div>
+    );
 };
 
 export default App;
---
+++ src/components/Tweet.tsx
@@ -0,0 +1,6 @@
+import styles from "./Tweet.module.scss";
+
+const Tweet = () => {
+    return <div className={styles.tweet} />;
+};
+export default Tweet;
