ad795fb21d51a7a5622a6f45f0bff1512f66e459
Bottersnike <bottersnike237@gmail.com>
2022-11-12 19:51:25 +0000

    Fix score state machine

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -3,13 +3,20 @@ import { Like } from "./Icons";
 
 import styles from "./Score.module.scss";
 
+const STATE_SHOW = 0;
+const STATE_NEXT = 1;
+const STATE_NOSHOW = 2;
+
 const Score = ({ score }: { score: number }) => {
-    const [anim, setAnim] = useState(0);
+    const [anim, setAnim] = useState(STATE_NOSHOW);
     useEffect(() => {
-        setAnim(1);
+        setAnim((old) => (score !== 0 ? STATE_NEXT : old));
     }, [score]);
     useEffect(() => {
-        if (anim) requestAnimationFrame(() => setAnim(0));
+        if (anim)
+            requestAnimationFrame(() =>
+                setAnim(anim === STATE_NEXT ? STATE_SHOW : STATE_NOSHOW)
+            );
     }, [anim]);
 
     return (
@@ -17,7 +24,10 @@ const Score = ({ score }: { score: number }) => {
             <div className={styles.score}>
                 {score ? score : 0}
                 <Like
-                    className={styles.popup + (anim ? "" : " " + styles.run)}
+                    className={
+                        styles.popup +
+                        (anim === STATE_SHOW ? " " + styles.run : "")
+                    }
                 />
             </div>
         </div>
