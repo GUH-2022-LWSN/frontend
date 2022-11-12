cfd2f05f239aec54b4a49808dfc1853ff399fd9c
Bottersnike <bottersnike237@gmail.com>
2022-11-12 14:24:21 +0000

    Create tweet component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,23 @@ import Tweet from "./components/Tweet";
 const App = () => {
     return (
         <div>
-            <Tweet />
+            <Tweet
+                tweet={{
+                    user: {
+                        displayName: "Test User",
+                        handle: "hi",
+                        pfp: "http://placekitten.com/48/48",
+                    },
+                    body: "Hi!!",
+                    attachment: "http://placekitten.com/1000/500",
+                    stats: {
+                        replies: 0,
+                        retweets: 0,
+                        likes: 0,
+                        date: "Just now"
+                    },
+                }}
+            />
         </div>
     );
 };
