97d4cc10b8d709bc72317e0875f2b6a52efde2e7
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2022-11-13 08:55:44 +0000

    Fix game over messages

--- src/components/GameOver.tsx
+++ src/components/GameOver.tsx
@@ -1,16 +1,16 @@
-import styles from './GameOver.module.scss'
+import styles from "./GameOver.module.scss";
 
 const messages = [
-    'Get ratio\'d',
-    'You got parody\'d',
-    'You won! (parody)',
-    'I\'m sorry, but Elon let you go',
-    'Your lost health like Lockheed Martin lost stocks',
-    'Enjoy your Lockheed Martini and dead stocks',
-    'Your lost health like Eli Lilly lost stocks',
-    'Poor performance tbh',
-    'You have failed your performance review',
-    'You\'ve been fired for working at home',
+    "Get ratio'd",
+    "You got parody'd",
+    "You won! (parody)",
+    "I'm sorry, but Elon let you go",
+    "You lost health like Lockheed Martin lost stocks",
+    "Enjoy your Lockheed Martini and dead stocks",
+    "You lost health like Eli Lilly lost stocks",
+    "Poor performance tbh",
+    "You have failed your performance review",
+    "You've been fired for working at home",
 ];
 
 interface GameOverProps {
@@ -20,16 +20,14 @@ interface GameOverProps {
 function GameOver(props: GameOverProps) {
     const { proceed } = props;
 
-    const selectedMessage = messages[Math.floor(Math.random() * messages.length)];
+    const selectedMessage =
+        messages[Math.floor(Math.random() * messages.length)];
 
     return (
         <div className={styles.gameOverWrapper}>
             <h1>Game Over!</h1>
             <h2>{selectedMessage}</h2>
-            <button
-                className={styles.goButton}
-                onClick={proceed}
-            >
+            <button className={styles.goButton} onClick={proceed}>
                 View Leaderboard
             </button>
         </div>
