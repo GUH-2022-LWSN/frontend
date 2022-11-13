f8b0c9896b5d85cdc5f40088eb6a849ae0bbbb28
Sam Hirst <45145746+Naeviant@users.noreply.github.com>
2022-11-13 03:17:35 +0000

    Show email at start of game

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Email from "./Email";
 import styles from "./Landing.module.scss";
 
 interface LandingProps {
@@ -9,50 +11,55 @@ interface LandingProps {
 function Landing(props: LandingProps) {
     const { start, twitterHandle, setTwitterHandle } = props;
 
+    const [emailVisible, setEmailVisible] = useState<boolean>(true);
+
     const sth = (value: string) => {
         setTwitterHandle(value.replaceAll("@", "").substring(0, 32));
     };
 
     return (
-        <div className={styles.landingWrapper}>
-            <img
-                alt="JustPhish Logo"
-                className={styles.logo}
-                src="/photo-white-512.png"
-            />
-            <h1 className={styles.welcomeText}>Welcome to JustPhish*</h1>
-            <sub>* Final name TBC!</sub>
-            <br />
-            <div className={styles.twitterHandleWrapper}>
-                <span className={styles.twitterHandlePrefix}>@</span>
-                <input
-                    className={styles.twitterHandleInput}
-                    onChange={(e) => {
-                        sth(e.target.value);
-                    }}
-                    placeholder="Your Twitter Handle"
-                    value={twitterHandle}
-                    onKeyDown={(e) => {
-                        if (e.code === "Enter") {
-                            if (twitterHandle !== "") start();
-                        }
-                    }}
-                    autoFocus
+        <>
+            <div className={styles.landingWrapper}>
+                <img
+                    alt="JustPhish Logo"
+                    className={styles.logo}
+                    src="/photo-white-512.png"
                 />
+                <h1 className={styles.welcomeText}>Welcome to JustPhish*</h1>
+                <sub>* Final name TBC!</sub>
+                <br />
+                <div className={styles.twitterHandleWrapper}>
+                    <span className={styles.twitterHandlePrefix}>@</span>
+                    <input
+                        className={styles.twitterHandleInput}
+                        onChange={(e) => {
+                            sth(e.target.value);
+                        }}
+                        placeholder="Your Twitter Handle"
+                        value={twitterHandle}
+                        onKeyDown={(e) => {
+                            if (e.code === "Enter") {
+                                if (twitterHandle !== "") start();
+                            }
+                        }}
+                        autoFocus
+                    />
+                </div>
+                <button
+                    className={styles.goButton}
+                    disabled={!twitterHandle}
+                    onClick={() => {
+                        if (twitterHandle !== "") start();
+                    }}
+                    style={{
+                        cursor: twitterHandle ? 'pointer' : 'default',
+                    }}
+                >
+                    Start Game
+                </button>
             </div>
-            <button
-                className={styles.goButton}
-                disabled={!twitterHandle}
-                onClick={() => {
-                    if (twitterHandle !== "") start();
-                }}
-                style={{
-                    cursor: twitterHandle ? 'pointer' : 'default',
-                }}
-            >
-                Start Game
-            </button>
-        </div>
+            <Email close={() => setEmailVisible(false)} visible={emailVisible} />
+        </>
     );
 }
 
